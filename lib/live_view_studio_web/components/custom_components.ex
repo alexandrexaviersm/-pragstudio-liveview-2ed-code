e6bb1a4324cfbf29330bfb1276f3eb97ec31f36c
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2023-02-20 18:56:47 -0300

    Start 10. Exercise - BoatsLive

--- lib/live_view_studio_web/components/custom_components.ex
+++ lib/live_view_studio_web/components/custom_components.ex
@@ -6,6 +6,11 @@ defmodule LiveViewStudioWeb.CustomComponents do
   slot :inner_block, required: true
 
   def promo(assigns) do
+    # For example, suppose minutes is declared as an optional attribute. And if a minutes value isn't passed to the promo component,
+    # only then do you want to assign one. Here's how to do that using assign_new:
+
+    # assigns = assign_new(assigns, :minutes, fn -> assigns.expiration * 60 end)
+
     ~H"""
     <div class="promo">
       <div class="deal">
