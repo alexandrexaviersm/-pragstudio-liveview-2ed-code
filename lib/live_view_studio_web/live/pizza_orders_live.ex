fff91da7f3e178831b1bc93b18f24728c6fc942e
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2023-02-28 17:17:03 -0300

    12. Exercise - Sort Pizza Orders 🍕

--- lib/live_view_studio_web/live/pizza_orders_live.ex
+++ lib/live_view_studio_web/live/pizza_orders_live.ex
@@ -5,11 +5,68 @@ defmodule LiveViewStudioWeb.PizzaOrdersLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
+    {:ok, socket, temporary_assigns: [pizza_orders: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = valid_sort_by(params)
+    sort_order = valid_sort_order(params)
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    pizza_orders = PizzaOrders.list_pizza_orders(options)
+
     socket =
       assign(socket,
-        pizza_orders: PizzaOrders.list_pizza_orders()
+        pizza_orders: pizza_orders,
+        options: options
       )
 
-    {:ok, socket}
+    {:noreply, socket}
   end
+
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/pizza-orders?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+      <%= sort_indicator(@sort_by, @options) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(:asc), do: :desc
+  defp next_sort_order(:desc), do: :asc
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order})
+       when column == sort_by do
+    case sort_order do
+      :asc -> "👆"
+      :desc -> "👇"
+    end
+  end
+
+  defp sort_indicator(_, _), do: ""
+
+  defp valid_sort_by(%{"sort_by" => sort_by})
+       when sort_by in ~w(id size style topping_1 topping_2 price) do
+    String.to_existing_atom(sort_by)
+  end
+
+  defp valid_sort_by(_params), do: :id
+
+  defp valid_sort_order(%{"sort_order" => sort_order})
+       when sort_order in ~w(asc desc) do
+    String.to_existing_atom(sort_order)
+  end
+
+  defp valid_sort_order(_params), do: :asc
 end
